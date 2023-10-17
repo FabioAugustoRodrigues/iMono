3684b16d86721aa69778c64b8233b463b57879d1
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2023-10-17 10:12:11 -0300

    created the env file -- (dotenv)

---
+++ app/config/source.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
+$dbHost = $_ENV['DB_HOST'];
+$dbUser = $_ENV['DB_USER'];
+$dbPass = $_ENV['DB_PASS'];
+$dbName = $_ENV['DB_NAME'];
