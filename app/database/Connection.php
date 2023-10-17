6fcf557aa933cde58574862d3b7103a4daeefae3
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2023-10-17 10:16:49 -0300

    created the database connection layer

---
+++ app/database/Connection.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\database;
+
+use \PDO;
+use PDOException;
+
+abstract class Connection
+{
+
+    private static $connection;
+
+    public static function getConnection()
+    {
+        if (self::$connection == null) {
+            try {
+                # Ambiente de produção
+                self::$connection = new PDO('mysql:host=' . $dbHost . ';dbname=' . $dbName . ';charset=utf8', $dbUser, $dbPass);
+
+                self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                echo "Error: " . $e->getMessage();
+            }
+        }
+
+        return self::$connection;
+    }
+
+    public static function disconnect()
+    {
+        self::$connection = null;
+    }
+}
