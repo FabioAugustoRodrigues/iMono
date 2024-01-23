87097e96cae6c2ee7e82491c2c462ea6df37c9d3
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2024-01-23 10:40:03 -0300

    Refactoring: Removed unused variables from the Method class

--- app/controller/http/ControllerRoutes.php
+++ app/controller/http/ControllerRoutes.php
@@ -17,13 +17,13 @@ class ControllerRoutes extends ControllerAbstract
     {
         self::$routes = array();
 
-        $this->addRoute("getCurrentDateTime", "app\\controller\\http\\API\\ExampleController", "getCurrentDateTime", false, false, null);
+        $this->addRoute("getCurrentDateTime", "app\\controller\\http\\API\\ExampleController", "getCurrentDateTime");
     }
 
-    public function addRoute($route, $class, $method, $needsAuth, $needsPermission, $codePermission = null)
+    public function addRoute($route, $class, $method)
     {
         if (!array_key_exists($route, self::$routes)) {
-            self::$routes[$route] = new Method($class, $method, $needsAuth, $needsPermission, $codePermission);
+            self::$routes[$route] = new Method($class, $method);
         }
     }
 
@@ -45,48 +45,33 @@ class ControllerRoutes extends ControllerAbstract
                 $middleware->before($post);
             }
 
-            $allowedRequest = !$method->getNeedsAuth();
-            if ($method->getNeedsAuth() && (isset($_SESSION["user_is_logged"]) && $_SESSION["user_is_logged"])) {
-                $allowedRequest = true;
-            }
+            $container = require_once __DIR__ . "../../../config/container.php";
 
-            if (!$method->authorize()) {
-                http_response_code(403);
-                return "Permission denied";
-            }
+            try {
+                $response = $container->call([self::$routes[$route]->getClass(), self::$routes[$route]->getMethod()], array($post));
 
-            if ($allowedRequest) {
-                $container = require_once __DIR__ . "../../../config/container.php";
-
-                try {
-                    $response = $container->call([self::$routes[$route]->getClass(), self::$routes[$route]->getMethod()], array($post));;
-
-                    foreach ($middlewares as $middleware) {
-                        $middleware->after($post, $response);
-                    }
-
-                    return $response;
-                } catch (ApplicationHttpException $applicationHttpException) {
-                    return $this->respondsWithData(
-                        $applicationHttpException->getMessage(),
-                        $applicationHttpException->getHttpStatusCode()
-                    );
-                } catch (ApplicationException $applicationException) {
-                    return $this->respondsWithData(
-                        $applicationException->getMessage(),
-                        500
-                    );
-                } catch (Exception $exception) {
-                    return $this->respondsWithData(
-                        "There was an error during the operation.",
-                        500
-                    );
+                foreach ($middlewares as $middleware) {
+                    $middleware->after($post, $response);
                 }
-            } else {
-                http_response_code(405);
-                return "Permission error";
+
+                return $response;
+            } catch (ApplicationHttpException $applicationHttpException) {
+                return $this->respondsWithData(
+                    $applicationHttpException->getMessage(),
+                    $applicationHttpException->getHttpStatusCode()
+                );
+            } catch (ApplicationException $applicationException) {
+                return $this->respondsWithData(
+                    $applicationException->getMessage(),
+                    500
+                );
+            } catch (Exception $exception) {
+                return $this->respondsWithData(
+                    "There was an error during the operation.",
+                    500
+                );
             }
-        }
+        } 
 
         http_response_code(404);
         return "Route not found";
@@ -102,17 +87,11 @@ class Method
 {
     private $class;
     private $method;
-    private $needsAuth;
-    private $needsPermission;
-    private $codePermission;
 
-    public function __construct($class, $method, $needsAuth, $needsPermission, $codePermission)
+    public function __construct($class, $method)
     {
         $this->class = $class;
         $this->method = $method;
-        $this->needsAuth = $needsAuth;
-        $this->needsPermission = $needsPermission;
-        $this->codePermission = $codePermission;
     }
 
     public function getClass()
@@ -123,30 +102,5 @@ class Method
     public function getMethod()
     {
         return $this->method;
-    }
-
-    public function getNeedsAuth()
-    {
-        return $this->needsAuth;
-    }
-
-    public function getNeedsPermission()
-    {
-        return $this->needsPermission;
-    }
-
-    public function getCodePermission()
-    {
-        return $this->codePermission;
-    }
-
-    public function authorize()
-    {
-        $allowed = !$this->getNeedsPermission();
-        if ($this->getNeedsPermission() && isset($_SESSION['permissions'][$this->getCodePermission()])) {
-            return true;
-        }
-
-        return $allowed;
     }
 }
