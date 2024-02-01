d96ccd72ce1081e2bd604e26abc06a5d3f567720
user <carlosdaniel@example.com>
2024-02-01 15:40:48 -0300

    fixing the request method in web.php

--- app/routes/web.php
+++ app/routes/web.php
@@ -5,4 +5,4 @@ namespace app\routes;
 use app\controller\http\ControllerRoutes;
 
 ControllerRoutes::get("/", "app\\controller\\http\\API\\ExampleController", "index");
-ControllerRoutes::get("/api/getCurrentDateTime", "app\\controller\\http\\API\\ExampleController", "getCurrentDateTime");
+ControllerRoutes::post("/api/getCurrentDateTime", "app\\controller\\http\\API\\ExampleController", "getCurrentDateTime");
