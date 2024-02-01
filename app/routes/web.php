5678a9a5f85660cb09bfe5833979cce4534af5b2
user <carlosdaniel@example.com>
2024-02-01 06:32:06 -0300

    solved sintax problem in web.php

--- app/routes/web.php
+++ app/routes/web.php
@@ -5,4 +5,4 @@ namespace app\routes;
 use app\controller\http\ControllerRoutes;
 
 ControllerRoutes::get("/", "app\\controller\\http\\API\\ExampleController", "index");
-ControllerRoutespost("/api/getCurrentDateTime", "app\\controller\\http\\API\\ExampleController", "getCurrentDateTime");
+ControllerRoutes::post("/api/getCurrentDateTime", "app\\controller\\http\\API\\ExampleController", "getCurrentDateTime");
