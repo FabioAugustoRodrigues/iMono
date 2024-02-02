3d307867f191aa8c052a3f6268459a27cadbd0f9
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2024-02-02 09:54:55 -0300

    use ::class constant instead of full class name

--- app/routes/web.php
+++ app/routes/web.php
@@ -2,7 +2,8 @@
 
 namespace app\routes;
 
+use app\controller\http\API\ExampleController;
 use app\controller\http\ControllerRoutes;
 
-ControllerRoutes::get("/", "app\\controller\\http\\API\\ExampleController", "index");
-ControllerRoutes::post("/api/getCurrentDateTime", "app\\controller\\http\\API\\ExampleController", "getCurrentDateTime");
+ControllerRoutes::get("/", ExampleController::class, "index");
+ControllerRoutes::post("/api/getCurrentDateTime", ExampleController::class, "getCurrentDateTime");
